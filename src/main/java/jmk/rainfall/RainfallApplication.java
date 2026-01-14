86428ee850221a4664fca9c50cdaae3381248ace
jeongmy <kjm9710613@naver.com>
2026-01-14 18:52:14 +0900

    init: rainfall

---
+++ src/main/java/jmk/rainfall/RainfallApplication.java
@@ -0,0 +1,13 @@
+package jmk.rainfall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainfallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainfallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jmk/rainfall/RainfallApplicationTests.java
@@ -0,0 +1,13 @@
+package jmk.rainfall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainfallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
